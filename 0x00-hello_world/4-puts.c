210fe4c61ac523ff41545a56f58b2afe335022a2
Kenneth Waiswa <wswkenneth7@hotmail.com>
2022-09-08 22:49:18 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -o cisfun $CFILE
+#include <stdio.h>
+
+/*
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ */
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
