2255ba78193bef337f0f9fb626b41a051465eafd
Kenneth Waiswa <wswkenneth7@hotmail.com>
2022-09-08 22:38:02 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * First c program in vi editor
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
