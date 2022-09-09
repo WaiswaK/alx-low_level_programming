453f1f487b72dcc3a3c8142ab1628895788e3616
Kenneth Waiswa <wswkenneth7@hotmail.com>
2022-09-09 06:18:13 -0700

    Task 2 fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,14 @@
  * Return: 0
  */
 int main(void)
-{	
-  char c = 'a';
-  while (c <= 'z')
-  {					
-	putchar(c);									c++;
-										  }
-putchar('\n');
-return (0);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
