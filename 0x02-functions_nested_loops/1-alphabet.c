1ececdc698cfffbee6daff08320fbb80290432dd
Kenneth Waiswa <wswkenneth7@hotmail.com>
2022-09-13 10:12:39 -0700

    Task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/*
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ * Description: A function that prints lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
